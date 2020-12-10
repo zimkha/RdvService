ae2621d65a164baa32eea6b558753bfe05616561
zimkhandiaye <zimkhandiaye@gmail.com>
2020-12-10 16:38:01 +0000

    update for the class AgendaCtrl

--- src/GraphQL/resolvers/AgendaResolver.js
+++ src/GraphQL/resolvers/AgendaResolver.js
@@ -1,13 +1,32 @@
 const { json } = require('body-parser');
 const Agenda = require('../../Models/Rdv.model')
+const moment = require('moment')
 class AgendaResolver {
     
     constructor(model) {
         this.model = Agenda;
     }
     
+    findCrenoExistCrenoByDate(fromH, toHour, day)
+    {
+        let exist = this.model.find({ 'day' : day, 'fromHour' : fromH, 'toHour' : toHour })
+                    .exec().sort('createdAt')
+                    then( agendas => {
+                        return agendas.toJSON()
+                    })
+                        .catch( (err) => {
+                            return err;
+                        })
+    }
     create(data)
     {
+        let fromTohour =  momennt(data.fromTohour).format('DD-MM-YYYY HH:mm');
+        let formOut    =  momennt(data.fromTo).format('DD-MM-YYYY HH:mm');
+        let day        =  momennt(data.fromTohour).format('DD-MM-YYYY HH:mm');
+
+
+
+        // On cherche dans la base si le docteur a déja une créno
         const record = new this.model(data);
             record.save()
                 .then( (agenda) => {
@@ -43,11 +62,31 @@ class AgendaResolver {
                          .catch((error) => {
                             return error;
                         });
-
     }
-    list(options = null)
-    {
 
+    list()
+    {
+        return this.model.find()
+        .sort('createdAt')
+        .exec()
+            .then(records => {
+                return records;
+        })
+            .catch(error => {
+                return error;
+            }); 
+    }
+    listByDoctor (dataDoctor)
+    {
+         this.model.find({'doctor.firstname' : dataDoctor.firstname, 'doctor.lastname' : dataDoctor.lastname, 'doctor.phone' : dataDoctor.phone }
+         )
+         .exec().sort('createdAt')
+            then( agendas => {
+                return agendas.toJSON()
+            })
+                .catch( (err) => {
+                    return err;
+                })
     }
 
 
